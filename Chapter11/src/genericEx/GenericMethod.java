d753cc72e30d865d5fce4f9084cadc16742f8e09
CHAEWON JEONG <chaewonJeong.dev@gmail.com>
2020-08-03 14:47:09 +0900

    Fix ERROR

---
+++ Chapter11/src/genericEx/GenericMethod.java
@@ -0,0 +1,8 @@
+package genericEx;
+
+public class GenericMethod {
+
+	public static <T,V> double makeRectangle(Point<T,V> p1, Point<T,V> p2) {
+		double left 
+	}
+}
---
+++ Chapter11/src/genericEx/Point.java
@@ -0,0 +1,17 @@
+package genericEx;
+
+public class Point<T,V> {
+	T x;
+	V y;
+	
+	
+	public T getX() {
+		return x;
+	}
+
+	public V getY() {
+		return y;
+	}
+
+	
+}
